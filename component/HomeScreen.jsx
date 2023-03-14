a1073129cac14a1b5fcdf70ac7e4068295d050f6
moudjane <mathis.oudjane@gmail.com>
2023-03-14 13:03:15 +0000

    HomeScreen 100%

--- component/HomeScreen.jsx
+++ component/HomeScreen.jsx
@@ -1,60 +1,99 @@
-import { Button, View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { Button, View, Text, Image, ScrollView } from 'react-native';
 import { StyleSheet } from 'react-native';
+import { Linking } from 'react-native';
+
 
 export function HomeScreen({ navigation }) {
+    const handlePress = () => {
+        Linking.openURL('https://diceboardgamelounge.com/');
+    };
     return (
-        <View style={styles.container}>
-            <View>
+        <ScrollView style={styles.container}>
+            <View style={styles.logoContainer}>
                 <Image style={styles.logo} source={require('../assets/Dice-dark-logo.png')} />
+            </View>
+            <Text style={styles.text}>Welcome to the <Text style={styles.bold}>DiceApp</Text> !</Text>
 
+            <View style={styles.logoContainer}>
+                <Image style={styles.image} source={require('../assets/dicetable.jpg')} />
             </View>
 
-            <View>
-                <Text>If you want more informations about Dice you can go to the website</Text>
+            <View style={styles.website}>
+                <Text style={styles.text}>If you want more informations about Dice you can go to the website</Text>
+                <Button
+                    color="#e9bd1f"
+                    title="Go to website"
+                    onPress={handlePress} />
             </View>
+
             <View style={styles.carouselContainer}>
-                <Text>Here you have the latest news at Dice:</Text>
+                <Text style={styles.text}>Here you have the latest news at Dice:</Text>
                 <ScrollView horizontal={true} pagingEnabled={true}>
                     <Image style={styles.carouselImage} source={require('../assets/jeu1.jpg')} />
                     <Image style={styles.carouselImage} source={require('../assets/dicee.jpg')} />
                     <Image style={styles.carouselImage} source={require('../assets/jeu1.jpg')} />
                 </ScrollView>
             </View>
+
             <View style={styles.quiz}>
-                <Text>ARE U READY FOR THE QUIZ ?</Text>
+                <Text style={styles.text}>ARE U READY FOR THE QUIZ ?</Text>
                 <Button style={styles.button}
                     title="Go to Quiz"
                     color="#e9bd1f"
                     onPress={() => navigation.navigate('Quiz')}
                 />
             </View>
-        </View>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center',
+        flexGrow: 1,
         backgroundColor: '#f5f5f5'
     },
     quiz: {
         alignItems: 'center',
         justifyContent: 'center',
+        margin: 20
+    },
+    logoContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
     },
     logo: {
-        width: 300,
-        height: 200
+        width: 150,
+        height: 100,
+        marginHorizontal: 10,
     },
     button: {},
     carouselContainer: {
-        height: 200,
+        height: 300,
         marginTop: 20,
     },
     carouselImage: {
         width: 300,
         height: 200,
     },
+    image: {
+        width: '95%',
+        height: 300
+    },
+    text: {
+        fontSize: 18,
+        textAlign: "center",
+        padding: 18,
+    },
+    bold: {
+        fontWeight: 'bold',
+        textAlign: "center",
+    },
+    website: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
 
 export default HomeScreen;
---
+++ readme.md
@@ -0,0 +1,9 @@
+
+
+# git clone
+
+# npm install
+
+# npm start
+
+And finally you can scan the QR code for test the App opn your own phone
